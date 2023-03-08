61ab2b47de0a8e0581ba443ab015648d2a281d7a
Noppasin Ruengrit <noppasin.r64@rsu.ac.th>
2023-03-08 16:01:10 +0700

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   host: "157.245.59.56",
   user: "u6403022",
   password: "6403022",
-  database: "u6403022",
+  database: "u6403022_Camera",
   port: 3366,
 });
 
@@ -21,65 +21,25 @@ app.get("/", function (req, res) {
   });
 });
 
-// app.get("/users", function (req, res) {
-//   connection.query("SELECT * FROM user", function (err, results) {
-//     console.log(results);
-//     res.json(results);
-//   });
-// });
-
-app.get("/pets", function (req, res) {
-  connection.query(
-    "SELECT pet.id , pet.petName AS petname, user.fullname AS owner FROM pet LEFT JOIN user ON pet.id = user.id",
-    function (err, results) {
-      console.log(results);
-      res.json(results);
-    }
-  );
+app.get("/customer", function (req, res) {
+  connection.query("SELECT * FROM a1_customer", function (err, results) {
+    console.log(results);
+    res.json(results);
+  });
 });
 
-app.post("/users", function (req, res) {
-  const email = req.body.email;
-  const fullname = req.body.fullname;
-  const city = req.body.city;
-  connection.query(
-    `INSERT INTO user (email, fullname, city) VALUES (?, ?, ?)`,
-    [email, fullname, city],
-    function (err, results) {
-      res.json(results);
-    }
-  );
+app.get("/order", function (req, res) {
+  connection.query("SELECT * FROM a1_order", function (err, results) {
+    console.log(results);
+    res.json(results);
+  });
 });
 
-app.get("/pets_price", function (req, res) {
-  connection.query(
-    `SELECT id, petName, price
-     FROM pet
-     ORDER BY price;`,
-    function (err, results) {
-      res.json(results);
-    }
-  );
-});
-
-app.get("/pets_price_chart", function (req, res) {
-  connection.query(
-    `SELECT id, petName, price
-     FROM pet
-     ORDER BY price;`,
-    function (err, results) {
-      const petNames = [];
-      const prices = [];
-      for (let i = 0; i < results.length; i++) {
-        petNames.push(results[i]["petName"]);
-        prices.push(parseFloat(results[i]["price"]));
-      }
-      res.json({
-        petNames,
-        prices,
-      });
-    }
-  );
+app.get("/product", function (req, res) {
+  connection.query("SELECT * FROM a1_product", function (err, results) {
+    console.log(results);
+    res.json(results);
+  });
 });
 
 app.listen(5000, () => {
